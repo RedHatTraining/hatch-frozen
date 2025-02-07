f19a4aea8fa75e297e7b977eff38fcec185ef390
Jaime Ramirez <jaime.ramirez@redhat.com>
2025-02-07 11:47:28 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,17 @@
+# `hatch-frozen`: A Hatch build hook plug-in for freezing dependencies
+
+## Usage
+
+TODO
+
+## Hacking
+
+The project uses `uv` as the package manager.
+
+To run the tests, use:
+
+    uv run pytest
+
+To verify the code style, use:
+
+    uvx ruff check
---
+++ tests/conftest.py
@@ -0,0 +1,39 @@
+import shutil
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
+
+import pytest
+
+
+@pytest.fixture
+def test_project():
+    source_dir = Path(__file__).resolve().parent / "_utils/test_project"
+    pyproject_toml = source_dir / "pyproject.toml"
+    plugin_root_dir = Path(__file__).resolve().parent.parent
+    # with open(pyproject_toml, "r") as f:
+    #     content = f.read()
+    #     new_content = content.replace(
+    #         "<<<hatch_frozen_cwd>>>", plugin_root_dir.as_uri()
+    #     )
+    # with open(pyproject_toml, "w") as f:
+    #     f.write(new_content)
+
+    # return source_dir
+
+    # Copy the test project to a tmp dir to make every test session independent
+    with TemporaryDirectory() as tmpdir:
+        # source_dir = Path(__file__).resolve().parent / "_utils/test_project"
+        tmp_project = Path(tmpdir) / "hatch-frozen-test-project"
+        shutil.copytree(source_dir, tmp_project)
+
+        pyproject_toml = source_dir / "pyproject.toml"
+        with open(pyproject_toml, "r") as f:
+            content = f.read()
+            new_content = content.replace(
+                "<<<hatch_frozen_cwd>>>", plugin_root_dir.as_uri()
+            )
+        with open(pyproject_toml, "w") as f:
+            f.write(new_content)
+
+        yield tmp_project
